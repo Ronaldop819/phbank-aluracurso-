4bee63471b99e0325cf11f9f9ee2f21efb85c2aa
Ronaldo P <datascience819@gmail.com>
2021-01-20 18:08:00 -0300

    Getters e Setters de referência

--- phbank-encapsulado/src/Conta.java
+++ phbank-encapsulado/src/Conta.java
@@ -57,5 +57,15 @@ class Conta {
 		public void setAgencia(int agencia) {
 			this.agencia = agencia;
 		}
+		
+		public void setTitular(Cliente titular) {
+			this.titular = titular;
+			
+		}
+		
+		public Cliente getTitular() {
+			return titular;
+		}
+		
 	
 }
--- phbank-encapsulado/src/TestaGetESet.java
+++ phbank-encapsulado/src/TestaGetESet.java
@@ -6,7 +6,24 @@ public class TestaGetESet {
 		Conta conta = new Conta();
 		conta.setNumero(1337);
 		System.out.println(conta.getNumero());
-
+		
+		Cliente ronaldo = new Cliente();
+//		conta.titular = ronaldo;
+		
+		ronaldo.setNome("Ronaldo Philippe");
+		conta.setTitular(ronaldo);
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("Programador");
+		System.out.println(conta.getTitular().getProfissao());
+//		agora em duas linhas
+		
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("Programador");
+		
+		System.out.println(titularDaConta);
+		System.out.println(ronaldo);
+		System.out.println(conta.getTitular());
 	}
 
 }
